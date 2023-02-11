f2a3803f8185b155f0adae65e63829530ef9b906
jhhow07 <jonathan.emanoel2204@gmail.com>
2023-02-11 13:49:56 -0300

    filter home page final

--- Pokedex/pokedex/src/views/Home.vue
+++ Pokedex/pokedex/src/views/Home.vue
@@ -1,15 +1,21 @@
 <template>
-    <div class="main_container">
-        <div @click="detailPage(index + 1)" v-for="(pokemon, index) in pokemonList" :key="index" class="pokemon_box">
-            <router-link :to="'/pokemonDetail' + (index + 1)">
-                <img class="pokemonImg" :src="getImg(index + 1)" alt="">
-                <p class="pokemonName">{{ pokemon.name }}</p>
-                <!-- <ul>
-                    <li v-for="type in pokemonInfo" :key="type.type.name">
-                        {{ type.id }}
-                    </li>
-                </ul> -->
-            </router-link>
+    <div>
+        <div class="filterBox">
+            <input v-model="filterValue" v-on:keyup="filter()" class="filterInput" placeholder="Pesquise pelo Pokémon..." type="text">
+        </div>
+        <div class="main_container">
+            <div @click="detailPage(index + 1)" v-for="(pokemon, index) in pokemonList" :key="index"
+                class="pokemon_box">
+                <router-link :to="'/pokemonDetail' + (index + 1)">
+                    <img class="pokemonImg" :src="getImg(index + 1)" alt="">
+                    <p class="pokemonName">{{ pokemon.name }}</p>
+                    <!-- <ul>
+                        <li v-for="type in pokemonInfo" :key="type.type.name">
+                            {{ type.id }}
+                        </li>
+                    </ul> -->
+                </router-link>
+            </div>
         </div>
     </div>
 </template>
@@ -23,7 +29,9 @@ export default {
     name: 'Home',
     data() {
         return {
-            pokemonList: []
+            pokemonList: [],
+            filterValue: '',
+            filteredList: []
         }
     },
     methods: {
@@ -38,12 +46,18 @@ export default {
         getImg(id) {
             return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
         },
-        detailPage(id) {
-            
+        filter() {
+            this.pokemonList = this.filteredList;
+            if(this.filterValue != '') {
+                this.pokemonList = this.filteredList.filter(x => x.name.includes(this.filterValue.toLowerCase()));
+            }
+            // this.filteredList = this.pokemonList.filter(x => x.name.toLowerCase() === this.filterValue.toLowerCase());
+            console.log(this.filteredList);
         }
     },
     async mounted() {
         this.pokemonList = await this.getPokemons();
+        this.filteredList = await this.getPokemons();
     },
 }
 </script>
@@ -60,6 +74,7 @@ export default {
     border-radius: 10px;
     padding: 3%;
 }
+
 .pokemon_box {
     display: flex;
     justify-content: center;
@@ -71,6 +86,7 @@ export default {
     border-radius: 15px;
     box-shadow: 0px 2px 3px 2px #00000052;
 }
+
 .pokemonName {
     color: white;
     text-shadow: 1px 3px 5px #000000ba;
@@ -78,6 +94,7 @@ export default {
     font-weight: normal;
     text-transform: capitalize;
 }
+
 .pokemonImg {
     background-color: #03030414;
     border-radius: 7%;
@@ -89,4 +106,19 @@ a {
     text-decoration: none !important;
 }
 
+.filterBox {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 2%;
+}
+
+.filterInput {
+    padding: 1%;
+    width: 320px;
+    font-size: 22px;
+    border-radius: 8px;
+    border: 2px solid #8282825c;
+    outline: none;
+}
 </style>
